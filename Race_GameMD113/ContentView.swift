60dce55d88a5be923ae49ee77fc0d1aa6fce38fa
DS4506 <davidsmith4506@gmail.com>
2025-10-25 10:24:04 -0500

    Initial Commit

---
+++ Race_GameMD113/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Race_GameMD113
+//
+//  Created by Willie Earl on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Race_GameMD113/Race_GameMD113App.swift
@@ -0,0 +1,17 @@
+//
+//  Race_GameMD113App.swift
+//  Race_GameMD113
+//
+//  Created by Willie Earl on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Race_GameMD113App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
